ab63756a8b54c3e254a918998e3c351eb3bf8d70
Yalikesifulei <alex.galganov@gmail.com>
2021-02-03 00:47:10 +0200

    started lab1

---
+++ lab1/code/Optimizer.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+class GDOptimizer:
+    def __init__(self, beta = 0.1, lmb = 0.5, tol = 1e-5, max_iter = 100):
+        """
+        Parameteres
+        -----------
+        beta, lmb : float
+                    parameteres of step decay,
+                    beta - initial step (default 0.1), 
+                    lmb - decay rate (default 0.5)
+        max_iter  : int
+                    maximum number of iterations, 
+                    default is 100
+        tol       : tolerance for algorithm stopping 
+                    |f(x_prev) - f(x_new)| < tol
+        """
+        self.beta = beta
+        self.lmb = lmb
+        self.tol = tol
+        self.max_iter = int(max_iter)
+    
+    def minimize(self, target_func, x0, h = 0.005):
+        """
+        Parameteres
+        -----------
+        target_func : callable
+                      function to minimize
+        x0          : np.array of shape (n, )
+                      initial point
+        h           : step for computing gradients,
+                      default is 0.005
+        -----------
+        Returns history - np.array with shape (n_iter, n+1), n - number of variables;
+                history[:, -1] - values of target functions
+                history[-1, :-1] - solution
+                history[-1, -1] - value of target function at solution point
+        """
+        def compute_grad(target_func, x, h):
+            n = len(x)
+            grad = np.zeros_like(x)
+            for i in range(n):
+                x_plus = np.copy(x)
+                x_plus[i] += h
+                x_minus = np.copy(x)
+                x_minus[i] -= h
+                grad[i] = (target_func(x_plus) - target_func(x_minus))/(2*h)
+            return grad
+
+        history = []
+        x = np.copy(x0)
+        history.append([*x0, target_func(x0)])      
+        for k in range(self.max_iter):
+            grad = compute_grad(target_func, x, h)
+            alpha = self.beta
+            while target_func(x - alpha*grad) >= target_func(x):
+                alpha = alpha*self.lmb
+            x = x - alpha*grad
+            history.append([*x, target_func(x)])
+            if k > 0 and np.abs(history[-1][1] - history[-2][1]) < self.tol:
+                break
+        return np.array(history)
---
+++ lab1/code/lab1.py
@@ -0,0 +1,26 @@
+import numpy as np
+from Optimizer import GDOptimizer
+from Plotter import PlotContour, PlotSurface
+
+def f(x_vect):
+    x, y = x_vect[0], x_vect[1]
+    return x**2 + 18*y**2 + 0.01*x*y + x - y
+
+opt_params = {
+    'beta' : 0.1,
+    'lmb' : 0.3,
+    'tol' : 1e-5,
+    'max_iter' : 100
+}
+opt = GDOptimizer(**opt_params)
+
+x0 = np.array([1, 1])
+hist = opt.minimize(f, x0)
+x_min, x_max = np.min(hist[:, 0])-0.2, np.max(hist[:, 0])+0.2
+y_min, y_max = np.min(hist[:, 1])-0.2, np.max(hist[:, 1])+0.2
+
+PlotContour((x_min, x_max), (y_min, y_max), f, fname = '../latex/pics/contour_init.png')
+PlotSurface((x_min, x_max), (y_min, y_max), f, fname = '../latex/pics/surface_init.png')
+
+PlotContour((x_min, x_max), (y_min, y_max), f, hist, fname = '../latex/pics/contour_final.png')
+PlotSurface((x_min, x_max), (y_min, y_max), f, hist, fname = '../latex/pics/surface_final.png')
