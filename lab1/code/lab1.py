ca04e0c6dd2b6295db6668f3d223187936c26aca
Yalikesifulei <alex.galganov@gmail.com>
2021-02-04 00:05:49 +0200

    finished lab

--- lab1/code/lab1.py
+++ lab1/code/lab1.py
@@ -2,10 +2,12 @@ import numpy as np
 from Optimizer import GDOptimizer
 from Plotter import PlotContour, PlotSurface
 
+# deifine target function
 def f(x_vect):
     x, y = x_vect[0], x_vect[1]
     return x**2 + 18*y**2 + 0.01*x*y + x - y
 
+# set optimizer parameteres and create it
 opt_params = {
     'beta' : 0.1,
     'lmb' : 0.3,
@@ -14,13 +16,14 @@ opt_params = {
 }
 opt = GDOptimizer(**opt_params)
 
+# initial point
 x0 = np.array([1, 1])
 hist = opt.minimize(f, x0)
-x_min, x_max = np.min(hist[:, 0])-0.2, np.max(hist[:, 0])+0.2
-y_min, y_max = np.min(hist[:, 1])-0.2, np.max(hist[:, 1])+0.2
 
+# save results
+x_min, x_max = np.min(hist[:, 0])-0.5, np.max(hist[:, 0])+0.5
+y_min, y_max = np.min(hist[:, 1])-0.5, np.max(hist[:, 1])+0.5
 PlotContour((x_min, x_max), (y_min, y_max), f, fname = '../latex/pics/contour_init.png')
 PlotSurface((x_min, x_max), (y_min, y_max), f, fname = '../latex/pics/surface_init.png')
-
 PlotContour((x_min, x_max), (y_min, y_max), f, hist, fname = '../latex/pics/contour_final.png')
 PlotSurface((x_min, x_max), (y_min, y_max), f, hist, fname = '../latex/pics/surface_final.png')
