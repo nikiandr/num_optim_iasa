74ce1fbf02d60893104c979f9690971d38dfe765
Oleksii Galganov <alex.galganov@gmail.com>
2021-03-24 19:29:33 +0200

    импортозамещение

--- lab3/code/Optimizer.py
+++ lab3/code/Optimizer.py
@@ -1,5 +1,52 @@
 import numpy as np
+import itertools
 
+def HyperplaneProj(point, p, beta):
+    """
+    Function for projecting argument point on X: (p, x) = beta
+    Parameters:
+    -----------
+    point   : point, that will be projected on X
+    p, beta : hyperplane parameters
+
+    Returns: projX(point)
+    """
+    return point + (beta - np.dot(p, point))/(np.linalg.norm(p)**2) * p
+
+def EllipsoidUniform(a, b, c, seed=42):
+    """
+    Select point uniformly on ellipsoid a*x**2 + b*y**2 + c*z**2 = 1
+    """
+    np.random.seed(seed)
+    theta = 2*np.pi*np.random.rand(1)
+    phi = np.arccos(2*np.random.rand(1) - 1)
+    x0 = np.array([1/np.sqrt(a) * np.cos(theta) * np.sin(phi),
+                   1/np.sqrt(b) * np.sin(theta) * np.sin(phi),
+                   1/np.sqrt(c) * np.cos(phi)]) # select initial point uniformly on Ellipsoid
+    np.random.seed(None)
+    return x0
+
+def EllipsoidProj(point, a, b, c, opt, alpha=100, seed=42):
+    """
+    Function for projecting argument point on X: a*x**2 + b*y**2 + c*z**2 <= 1
+    using penalty function method
+    Parameters:
+    -----------
+    point       : point, that will be projected on X
+    a, b, c     : ellipsoid parameters
+    opt         : optimizer, callable
+
+    Returns: projX(point)
+    """
+    if a*point[0]**2 + b*point[1]**2 + c*point[2]**2 <= 1:
+        return point
+    else:
+        x0 = EllipsoidUniform(a, b, c, seed)
+        def penalizer(vect):
+            x, y, z = vect[0], vect[1], vect[2]
+            return (x-point[0])**2 + (y-point[1])**2 + (z-point[2])**2 + alpha*(a*x**2 + b*y**2 + c*z**2 - 1)**2
+        proj = opt.minimize(penalizer, x0)[-1, :-1]
+        return proj.flatten().tolist()
 
 class GDProjOptimizer:
     def __init__(self, beta=0.1, lmb=0.5, tol=1e-5, max_iter=100):
@@ -57,15 +104,93 @@ class GDProjOptimizer:
         for k in range(self.max_iter):
             grad = compute_grad(target_func, x, h)
             alpha = self.beta
-            """print('-----', k)
-            print('x =', x)
-            print('grad =', grad)
-            print('alpha =', alpha)
-            print('x - alpha*grad =', x-alpha*grad)
-            print('proj =', proj_func(x - alpha*grad))"""
-            #while target_func(proj_func(x - alpha*grad)) >= target_func(x) and alpha > 1e-7:
-            #    alpha = alpha*self.lmb
+            while target_func(proj_func(x - alpha*grad)) >= target_func(x) and alpha > 1e-7:
+                alpha = alpha*self.lmb
             x = proj_func(x - alpha*grad)
+            history.append([*x, target_func(x)])
+            if k > 0 and np.abs(history[-1][1] - history[-2][1]) < self.tol:
+                break
+        return np.array(history)
+
+class NewtonOptimizer:
+    def __init__(self, beta=1, lmb=None, tol=1e-5, max_iter=100):
+        """
+        Parameteres
+        -----------
+        beta      : float
+                    initial step (default 1)
+        lmb       : float or None
+                    step decay parameter
+                    if None - classic Newton method is used instead
+        max_iter  : int
+                    maximum number of iterations,
+                    default is 100
+        tol       : tolerance for algorithm stopping
+                    |f(x_prev) - f(x_new)| < tol
+        """
+        self.beta = beta
+        self.tol = tol
+        self.max_iter = int(max_iter)
+        self.lmb = lmb
+
+    def minimize(self, target_func, x0, h=0.005):
+        """
+        Parameteres
+        -----------
+        target_func : callable
+                      function to minimize
+        x0          : np.array of shape (n, )
+                      initial point
+        h           : step for computing gradients,
+                      default is 0.005
+        -----------
+        Returns history - np.array with shape (n_iter, n+1),
+                          n - number of variables;
+                history[:, -1] - values of target functions
+                history[-1, :-1] - solution
+                history[-1, -1] - value of target function at solution point
+        """
+        def compute_grad(target_func, x, h):
+            n = len(x)
+            grad = np.zeros_like(x)
+            for i in range(n):
+                x_plus = np.copy(x)
+                x_plus[i] += h
+                x_minus = np.copy(x)
+                x_minus[i] -= h
+                grad[i] = (target_func(x_plus) - target_func(x_minus))/(2*h)
+            return grad
+
+        def compute_hessian(target_func, x, h):
+            n = len(x)
+            hessian = np.empty((n, n))
+            for k in range(n):
+                for m in range(k+1):
+                    dx = np.zeros_like(x)
+                    dx[k] = h/2
+                    dy = np.zeros_like(x)
+                    dy[m] = h/2
+                    hessian[k, m] = sum([i*j * target_func(x + i*dx + j*dy)
+                                        for i, j in itertools.product([1, -1],
+                                                                      repeat=2)
+                                        ]) / h**2
+                    hessian[m, k] = hessian[k, m]
+            return hessian
+
+        history = []
+        x = np.copy(x0)
+        history.append([*x0, target_func(x0)])
+        for k in range(self.max_iter):
+            grad = compute_grad(target_func, x, h)
+            hessian = compute_hessian(target_func, x, h)
+            step = np.linalg.pinv(hessian) @ grad
+
+            alpha = self.beta
+            if self.lmb is not None:
+                while target_func(x - alpha*step) > target_func(x):  # > !!!
+                    alpha = alpha * self.lmb
+                    
+            x = x - alpha * step
             history.append([*x, target_func(x)])
             if k > 0 and np.abs(history[-1][1] - history[-2][1]) < self.tol:
                 break
--- lab3/code/lab3.py
+++ lab3/code/lab3.py
@@ -1,7 +1,7 @@
 import numpy as np
-from Optimizer import GDProjOptimizer
+from Optimizer import GDProjOptimizer, NewtonOptimizer
+from Optimizer import HyperplaneProj, EllipsoidProj
 from Plotter import Ellipsoid, Hyperplane, make_plane, make_ellipsoid
-from scipy.optimize import minimize
 
 
 def f1(x_vec):
@@ -12,63 +12,32 @@ def f9(x_vec):
     x, y, z = x_vec[0], x_vec[1], x_vec[2]
     return x + 4*y + z
 
-def HyperplaneProj(point, p, beta):
-    """
-    Function for projecting argument point on X: (p, x) = beta
-    Parameters:
-    -----------
-    point   : point, that will be projected on X
-    p, beta : hyperplane parameters
-
-    Returns: projX(point)
-    """
-    return point + (beta - np.dot(p, point))/(np.linalg.norm(p)**2) * p
-
-def EllipsoidProj(point, a, b, c, alpha=100):
-    """
-    Function for projecting argument point on X: a*x**2 + b*y**2 + c*z**2 <= 1
-    Parameters:
-    -----------
-    point   : point, that will be projected on X
-    a, b c  : ellipsoid parameters
-
-    Returns: projX(point)
-    """
-    if a*point[0]**2 + b*point[1]**2 + c*point[2]**2 <= 1:
-        return point
-    else:
-        def penalizer(vect):
-            x, y, z = vect[0], vect[1], vect[2]
-            return (x-point[0])**2 + (y-point[1])**2 + (z-point[2])**2 + alpha*(a*x**2 + b*y**2 + c*z**2 - 1)**2
-        proj = minimize(penalizer, x0=np.random.randn(3)).x
-        return proj
-
 # set optimizer parameteres and create it
 opt_params = {
-    'beta': 0.1,
+    'beta': 1,
     'lmb': 0.5,
     'tol': 1e-5,
     'max_iter': 100
 }
 opt = GDProjOptimizer(**opt_params)
 
-hist_var1 = opt.minimize(f1,
+"""hist_var1 = opt.minimize(f1,
                     lambda x: HyperplaneProj(x, np.array([1, 1, 1]), 1),
-                    x0=1000*np.array([1, 1, 1]))
+                    x0=1/3*np.array([1, 1, 1]))
 x_min, x_max = np.min(hist_var1[:, 0])-0.5, np.max(hist_var1[:, 0])+0.5
 y_min, y_max = np.min(hist_var1[:, 1])-0.5, np.max(hist_var1[:, 1])+0.5
 
 make_plane((x_min, x_max), (y_min, y_max), 
            target_func=lambda x, y, z: Ellipsoid(1, 1, 1, x, y, z),
            p=np.array([1, 1, 1]), beta=1, points=hist_var1,
-           fname='./lab3/latex/pics/res_var1_(1000).png')
+           fname='./lab3/latex/pics/res_var1_(argmin).png')"""
 
+proj_opt = NewtonOptimizer(**opt_params)
 hist_var9 = opt.minimize(f9,
-                    lambda x: EllipsoidProj(x, 1, 3, 2),
+                    lambda x: EllipsoidProj(x, 1, 3, 2, proj_opt),
                     x0=1000*np.array([1, 1, 1]))
 x_min, x_max = np.min(hist_var9[:, 0])-0.5, np.max(hist_var9[:, 0])+0.5
 y_min, y_max = np.min(hist_var9[:, 1])-0.5, np.max(hist_var9[:, 1])+0.5
-
 
 make_ellipsoid(target_func=lambda x, y, z: Hyperplane([1, 4, 1], 0, x, y, z),
               a=1, b=3, c=2, points=hist_var9,
