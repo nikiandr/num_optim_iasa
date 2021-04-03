23b856c8b65396f26d37dfcef29914d317d38393
Andrii Yerko <aerko12@gmail.com>
2021-04-03 13:37:50 +0300

    Lab4 dichotomy + golden ratio

--- lab4/code/Optimizer.py
+++ lab4/code/Optimizer.py
@@ -14,3 +14,24 @@ def GradDesc(f, x0, beta=1, lmb=0.5, tol=1e-5, max_iter=1000, h=0.005):
         if np.abs(D(f, x, h)) < tol:
             break
     return {'x': x, 'iterations': i+1}
+
+
+def dichotomy(f, a, b, delta, tol=1e-5, max_iter=1000):
+    for i in range(max_iter):
+        d = (b - a)/2 * delta
+        x1 = (a + b)/2 - d
+        x2 = (a + b)/2 + d
+        if f(x1) < f(x2):
+            b = x2
+        else:
+            a = x1
+        if abs(b-a) < 2*tol:
+            break
+    return {'x': (a+b)/2, 'iterations': i+1}
+
+
+def golden_ratio(f, a, b, tol=1e-5, max_iter=1000):
+    d = 4/(5**0.5 + 1) - 1
+    return dichotomy(f, a, b, d, tol, max_iter)
+
+
--- lab4/code/lab4.py
+++ lab4/code/lab4.py
@@ -1,6 +1,8 @@
 import numpy as np
-from Optimizer import GradDesc
+from Optimizer import GradDesc, dichotomy, golden_ratio
 
 f = lambda x: np.exp(x) - 0.33*x**3 + 2*x
 
 print(GradDesc(f, x0=2, beta=0.5))
+print(dichotomy(f, -1, -2, 0.001))
+print(golden_ratio(f, -1, -2))
