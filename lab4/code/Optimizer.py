c6e848bb650837643189c6527da9a2ffa026fd33
nikiandr <nikita.forduy@protonmail.com>
2021-04-05 01:00:58 +0300

    Fibogacci method

--- lab4/code/Optimizer.py
+++ lab4/code/Optimizer.py
@@ -1,4 +1,19 @@
+from typing import NoReturn
 import numpy as np
+
+def chad_fibonacci(i):
+    a = 1
+    b = 1
+    for __ in range(i):
+        a, b = b, a + b
+    return a
+
+
+def virgin_fibonacci(i):
+    left = ((1 + (5 ** 0.5))/2)**(i+1)
+    right = ((1 - (5 ** 0.5))/2)**(i+1)
+    return (left - right)/ (5 ** 0.5)
+
 
 def D(f, x, h=0.005):
     return (f(x+h) - f(x-h))/(2*h)
@@ -35,3 +50,25 @@ def golden_ratio(f, a, b, tol=1e-5, max_iter=1000):
     return dichotomy(f, a, b, d, tol, max_iter)
 
 
+def fibonacci_method(f, a, b, n):
+    # Fn >= (b - a)/tol
+    '''
+    n = 0
+    while chad_fibonacci(n) < (b - a) / tol:
+        n += 1
+    '''
+    x1 = a + (chad_fibonacci(n-2)/chad_fibonacci(n))*(b - a)
+    x2 = a + (chad_fibonacci(n-1)/chad_fibonacci(n))*(b - a)
+    for k in range(1, n - 1):
+        if f(x1) > f(x2):
+            a = x1
+            x1 = x2
+            x2 = a + (chad_fibonacci(n-k-1)/chad_fibonacci(n-k))*(b - a)
+        else:
+            b = x2
+            x2 = x1
+            x1 = a + (chad_fibonacci(n-k-2)/chad_fibonacci(n-k))*(b - a)
+    delta = 1e-3 # smoll boi
+    a = 0.5*(b - a) + a
+    b = (0.5 + delta)*(b - a) + a
+    return {'x': (a+b)/2, 'iterations': n}
--- lab4/code/lab4.py
+++ lab4/code/lab4.py
@@ -1,8 +1,9 @@
 import numpy as np
-from Optimizer import GradDesc, dichotomy, golden_ratio
+from Optimizer import GradDesc, dichotomy, golden_ratio, fibonacci_method
 
 f = lambda x: np.exp(x) - 0.33*x**3 + 2*x
 
 print(GradDesc(f, x0=2, beta=0.5))
 print(dichotomy(f, -1, -2, 0.001))
 print(golden_ratio(f, -1, -2))
+print(fibonacci_method(f, -1, -2, 100))
