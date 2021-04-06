da38ad89bbb86c2441967a1858384d6462d9bf63
Oleksii Galganov <alex.galganov@gmail.com>
2021-04-06 10:28:25 +0300

    it really works

--- lab5/code/Optimizer.py
+++ lab5/code/Optimizer.py
@@ -1,5 +1,4 @@
 import numpy as np
-from GradientOptimizer import GDProjOptimizer, OrthantProjector
 
 def gradient(target_func, x, h=0.005):
     n = len(x)
@@ -12,38 +11,50 @@ def gradient(target_func, x, h=0.005):
         grad[i] = (target_func(x_plus) - target_func(x_minus))/(2*h)
     return grad
 
-def QuadraticOptimizer(C, p, A, b, N_avg=20):
+def NonnegativeQuadraticOptimizer(A, b):
     """
-    Solve 1/2 (C*x, x) + (p, x) -> min given A*x >= b
-    (C is symmetric matrix)
+    Solve 1/2 * (Av, v) + (b, v) -> min under v >= 0
+    """
+    A_plus, A_minus = np.maximum(0, A), -np.minimum(0, A)
+    v = np.random.rand(len(b))
+    v_temp = v.copy()
+    for _ in range(100):
+        Ap_v, Am_v = A_plus @ v, A_minus @ v
+        for i in range(len(v)):
+            if np.abs(Ap_v[i]) >= 1e-8:
+                v_temp[i] = v[i] * (-b[i] + np.sqrt(b[i]**2 + 4*Ap_v[i]*Am_v[i]))/(2*Ap_v[i])
+            else:
+                v = np.random.rand(len(b))
+                break
+        if np.linalg.norm(v - v_temp) < 1e-7:
+            v = v_temp.copy()
+            break
+        v = v_temp.copy()
+    return v
+
+def GeneralQuadraticOptimizer(p, A, b, N_avg=15):
+    """
+    Solve 1/2 * ||x||^2 + (p, x) -> min under A * x >= b
     using dual problem:
-        -1/2 (C*u, u) + (b, v) -> max given
-        A.T*v - C*u = p
+        1/2 * ||u||^2 - (b, v) -> min under
+        A.T * v - u = p
         v >= 0
     solution in u == solution in x
-    N_avg is number of averaged solutions from random points.
     """
-    # A.T*v - C*u = p <=> A.T*v - p = C*u
-    # u = C^{-1} * (A.T*v - p)
-    C_inv = np.linalg.pinv(C)
-    def dual_target(v):
-        u = C_inv @ (A.T @ v - p)
-        return 1/2 * np.dot(C @ u, u) - np.dot(b, v)
-    # explicit form of dual_target gradient
-    def dual_grad(v):
-        return (A.T @ v - p).T @ (C_inv @ A.T) - b
-    opt = GDProjOptimizer(beta=1, lmb=0.5, tol=1e-7, max_iter=1000)
-    res_v = []
-    for _ in range(N_avg):
-        v0 = OrthantProjector(0.01*np.random.randn(len(b)))
-        v = opt.minimize(dual_target, OrthantProjector, v0, exact_grad=dual_grad)[-1, :-1]
-        res_v.append(v)
-    v = np.array(res_v).mean(axis=0)
-    u = C_inv @ (A.T @ v - p)
-    return u
+    # dual target: u = A.T @ v - p, so it equals
+    # 1/2*(AA.T*v, v) - (A*p + b, v) + ||p||^2 -> min, v >= 0
+    v = NonnegativeQuadraticOptimizer(A @ A.T, -A @ p - b)
+    u = A.T @ v - p
+    return u, v
 
-def LinearizationOptimizer(f0, constraints, x0, eps, N, tol=1e-5, max_iter=100):
-    def F_N(x):
+def LinearizationOptimizer(f0, constraints, x0, eps, tol=1e-5, max_iter=100):
+    """
+    Solve f0(x) -> min under f_i(x) <= 0 for f_i in constraints
+    using linearization method
+    0 < eps < 1 is method parameter
+    """
+    N_k = 100
+    def F_N(x, N):
         return f0(x) + N*max([0] + [f(x) for f in constraints])
     history = []
     x = np.copy(x0).astype(float)
@@ -57,13 +68,15 @@ def LinearizationOptimizer(f0, constraints, x0, eps, N, tol=1e-5, max_iter=100):
             constr_val.append(constraints[i](x))
         d_constr = np.array(d_constr)
         constr_val = np.array(constr_val)
-        p = QuadraticOptimizer(np.eye(len(df0)), df0, -d_constr, constr_val)
+        p, v = GeneralQuadraticOptimizer(df0, -d_constr, constr_val)
         alpha = 1
-        while F_N(x + alpha*p) > F_N(x) - eps*alpha*np.linalg.norm(p)**2:
+        while F_N(x + alpha*p, N_k) > F_N(x, N_k) - eps*alpha*np.linalg.norm(p)**2:
             alpha = alpha * 0.5
         x = x + alpha*p
+        if v.sum() > N_k:
+            N_k = 2 * v.sum()
         history.append([*x, f0(x)])
-        if k > 0 and np.abs(history[-1][1] - history[-2][1]) < tol:
+        if k > 0 and np.linalg.norm(p) < tol:
                 break
     return np.array(history)
 
@@ -76,6 +89,6 @@ def f1(x_vect):
     x, y = x_vect[0], x_vect[1]
     return -(-x**2 + y)
 
-x0 = np.array([1.2, -1.3]).astype(float)
-hist = LinearizationOptimizer(f0, [f1], x0, eps=0.5, N=100, max_iter=100)
+x0 = np.random.randn(2)
+hist = LinearizationOptimizer(f0, [f1], x0, eps=0.5, max_iter=1000)
 print(hist[-1, :-1])
